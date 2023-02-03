609785867fbc059a8d83fae5773a12e5d9b43b34
unknown <ripsli@hotmail.com>
2023-02-03 16:41:29 +0100

    mssc-brewery tutorial

--- src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -1,2 +1,15 @@
-package guru.springframework.msscbrewery.services;public interface CustomerService {
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+
+import java.util.UUID;
+
+public interface CustomerService {
+    CustomerDto getCustomerById(UUID customerId);
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+
+    void deleteById(UUID customerId);
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,2 +1,37 @@
-package guru.springframework.msscbrewery.services;public class CustomerServiceImpl {
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public CustomerDto getCustomerById(UUID customerId){
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Joe Black")
+                .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl - would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer...");
+    }
 }
